d6705eac7cbd3001bd1b2ec1941479f40a08b685
UbadahJ <ubadahjafry@gmail.com>
2020-05-08 00:55:21 +0500

    Added basic virtual server class with status enums

--- src/vir_server.py
+++ src/vir_server.py
@@ -1,9 +1,22 @@
+from enum import Enum
+
+Status = Enum(
+    "Status", {"IDLE": "idle", "PROCESSING": "processing", "QUITING": "quiting"}
+)
+
+
 class Server:
+    def __init__(self, file, *, id: int, interval: int, port: int):
+        self.file = file
+        self.id = id
+        self.interval = interval
+        self.port = port
+        self.status = Status.IDLE
+        while self.status != Status.QUITING:
+            pass
 
+    def update(self):
+        print("Server {} at port {}: Status {}".format(self.id, self.port, self.status))
 
-
-    def __init__(self):
-        pass
-
-def start(file, *, port: int):
-    pass
+    def kill(self):
+        self.status = Status.QUITING
