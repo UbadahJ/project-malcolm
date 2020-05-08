a1f410726b91ab672c5bc63170e07705a7e829b1
UbadahJ <ubadahjafry@gmail.com>
2020-05-08 07:07:08 +0500

    Added function to generate checksum (SHA-1)

--- src/fileutils.py
+++ src/fileutils.py
@@ -1,9 +1,17 @@
 from typing import List
+from hashlib import sha1
 import os
+
 
 def spilt(file: str, parts: int) -> List[int]:
     total = get_size(file)
     return [i for i in range(0, total - 1, round(total / parts))]
 
-def get_size(file: str) -> int: 
+
+def get_size(file: str) -> int:
     return os.path.getsize(file)
+
+
+def gen_checksum(file: str) -> str:
+    with open(file, "rb") as f:
+        return sha1(f.read()).hexdigest()
