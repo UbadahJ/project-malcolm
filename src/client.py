7dd3e609ff88df53a4ffd3e4abb7a5e878939b42
UbadahJ <ubadahjafry@gmail.com>
2020-05-10 09:02:41 +0500

    Fixed wrong parameters being passed to object

--- src/client.py
+++ src/client.py
@@ -89,4 +89,4 @@ def verify(args: argparse.Namespace):
 
 if __name__ == "__main__":
     args = verify(parse_args())
-    Client(args.output, args.address, args.port, args.resume)
+    Client(address=args.address, output=args.output, ports=args.port, resume=args.resume)
