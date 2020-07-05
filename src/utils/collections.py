7e62a795c9dbf4418cd6743eef1e87504f04fa1b
UbadahJ <ubadahjafry@gmail.com>
2020-07-05 15:24:39 +0500

    Fixed argument data type

--- src/utils/collections.py
+++ src/utils/collections.py
@@ -1,4 +1,4 @@
-from typing import Sequence, TypeVar, Optional, List
+from typing import Sequence, TypeVar, Optional, List, Union
 
 __T = TypeVar('__T')
 
@@ -17,11 +17,11 @@ def last(data: Sequence[__T]) -> Optional[__T]:
         return None
 
 
-def flatten(data: Sequence[Sequence[__T]]) -> Sequence[__T]:
+def flatten(data: Sequence[Union[Sequence[__T], __T]]) -> Sequence[__T]:
     _list: List[__T] = list()
     for item in data:
         if isinstance(item, Sequence):
             _list.extend(item)
         else:
-            _list.append(item)  # type: ignore
+            _list.append(item)
     return _list
