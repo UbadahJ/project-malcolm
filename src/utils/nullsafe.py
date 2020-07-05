a42035be35efb6dc4a2a2b6ff55e4448a08719c6
UbadahJ <ubadahjafry@gmail.com>
2020-07-06 02:03:01 +0500

    Added function to assert data type

--- src/utils/nullsafe.py
+++ src/utils/nullsafe.py
@@ -1,4 +1,4 @@
-from typing import Union, Optional, TypeVar
+from typing import Union, Optional, TypeVar, Type
 
 __T1 = TypeVar('__T1')
 __T2 = TypeVar('__T2')
@@ -12,5 +12,10 @@ def ifnoneorelse(value: Optional[__T1], onelse: __T2) -> Union[__T1, __T2]:
 
 def assertnotnone(value: Optional[__T1]) -> __T1:
     assert value is not None
+    return value
+
+
+def asserttype(type_: Type[__T1], value: __T2) -> __T1:
+    assert isinstance(value, type_)
     return value
 
