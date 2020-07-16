fe0994e7a0cc7ba9ae2827ed37d665a94e15d13b
UbadahJ <ubadahjafry@gmail.com>
2020-07-16 17:42:30 +0500

    Unicode data is encoded as Base64 to allow saving to json

--- src/serialization/client.py
+++ src/serialization/client.py
@@ -1,11 +1,16 @@
+import base64
 from typing import Dict, Union
 
 from utils.collections import first
-from utils.nullsafe import notnone
 
 
 def dump_hook(instance) -> Dict[str, Union[int, str, None]]:
+    _data = list()
+    for (start, end), seg in instance.data_unfinished:
+        if seg is not None:
+            seg = base64.b64encode(seg).decode('utf-8')
+        _data.append(((start, end), seg))
     return {
-        "checks":          notnone(first(instance.checks)),
-        "unfinished_data": instance.data_unfinished
+        "checks":          first(instance.checks),
+        "unfinished_data": _data
     }
